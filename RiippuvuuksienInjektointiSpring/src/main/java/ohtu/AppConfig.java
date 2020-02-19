3bf2cc703bca22777f4ae63ab9d667ffffd3b410
Ville Kinnula <kinnula.ville@gmail.com>
2020-02-19 12:07:31 +0200

    viikko2

---
+++ RiippuvuuksienInjektointiSpring/src/main/java/ohtu/AppConfig.java
@@ -0,0 +1,8 @@
+package ohtu;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan(basePackages = "ohtu.laskin")
+public class AppConfig  {}
---
+++ RiippuvuuksienInjektointiSpring/src/main/java/ohtu/Main.java
@@ -0,0 +1,35 @@
+package ohtu;
+
+import ohtu.laskin.Laskin;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+public class Main {
+
+    public static void main(String[] args) {
+        ApplicationContext ctx = new AnnotationConfigApplicationContext(AppConfig.class);
+        
+        Laskin laskin = ctx.getBean(Laskin.class);
+        laskin.suorita();
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//        //Koe k = ctx.getBean(Koe.class);
+//        Koe k = (Koe)ctx.getBean("koe");
+//        k.foo();
+//        
+//        IO io = ctx.getBean(IO.class);
+//        io.print("paska");
